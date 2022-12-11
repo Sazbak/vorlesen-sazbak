3c2f5351fb70692e7a56e23d90cc427605de5575
Balazs Szabo <sazbak@gmail.com>
2022-12-11 23:54:49 +0100

    Added Poppins font to custom utilitiy classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ const typographyPlugin = plugin(
 
     addUtilities(
       Object.entries(typography).map(
-        ([className, [fontSize, lineHeight, fontWeight]]) => ({
+        ([className, [fontSize, lineHeight, fontWeight, fontFamily]]) => ({
           [`.${e(className)}`]: {
             fontSize,
             lineHeight,
-            fontWeight
+            fontWeight,
+            fontFamily
           }
         })
       )
@@ -19,28 +20,28 @@ const typographyPlugin = plugin(
   {
     theme: {
       typography: {
-        "headline-xl-500": [58, 66.7 / 58, 500],
-        "headline-xl-400": [58, 66.7 / 58, 400],
-        "headline-l-600": [46, 52.9 / 46, 600],
-        "headline-l-400": [46, 52.9 / 46, 400],
-        "headline-m-600": [33, 37.95 / 33, 600],
-        "headline-m-500": [33, 37.95 / 33, 500],
-        "headline-m-400": [33, 37.95 / 33, 400],
-        "headline-s-600": [23, 26.45 / 23, 600],
-        "headline-s-500": [23, 26.45 / 23, 500],
-        "headline-s-400": [23, 26.45 / 23, 400],
-        "headline-xs-600": [19, 21.85 / 19, 600],
-        "headline-xs-500": [19, 21.85 / 19, 500],
+        "headline-xl-500": [58, 66.7 / 58, 500, "Poppins"],
+        "headline-xl-400": [58, 66.7 / 58, 400, "Poppins"],
+        "headline-l-600": [46, 52.9 / 46, 600, "Poppins"],
+        "headline-l-400": [46, 52.9 / 46, 400, "Poppins"],
+        "headline-m-600": [33, 37.95 / 33, 600, "Poppins"],
+        "headline-m-500": [33, 37.95 / 33, 500, "Poppins"],
+        "headline-m-400": [33, 37.95 / 33, 400, "Poppins"],
+        "headline-s-600": [23, 26.45 / 23, 600, "Poppins"],
+        "headline-s-500": [23, 26.45 / 23, 500, "Poppins"],
+        "headline-s-400": [23, 26.45 / 23, 400, "Poppins"],
+        "headline-xs-600": [19, 21.85 / 19, 600, "Poppins"],
+        "headline-xs-500": [19, 21.85 / 19, 500, "Poppins"],
 
-        "paragraph-l-500": [19, 25.65 / 19, 500],
-        "paragraph-l-400": [19, 25.65 / 19, 400],
-        "paragraph-m-700": [15, 20.25 / 15, 700],
-        "paragraph-m-500": [15, 20.25 / 15, 500],
-        "paragraph-m-400": [15, 20.25 / 15, 400],
-        "paragraph-s-700": [13, 17.55 / 13, 700],
-        "paragraph-s-500": [13, 17.55 / 13, 500],
-        "paragraph-s-400": [13, 17.55 / 13, 400],
-        "paragraph-xs-500": [11, 14.85 / 11, 500]
+        "paragraph-l-500": [19, 25.65 / 19, 500, "Poppins"],
+        "paragraph-l-400": [19, 25.65 / 19, 400, "Poppins"],
+        "paragraph-m-700": [15, 20.25 / 15, 700, "Poppins"],
+        "paragraph-m-500": [15, 20.25 / 15, 500, "Poppins"],
+        "paragraph-m-400": [15, 20.25 / 15, 400, "Poppins"],
+        "paragraph-s-700": [13, 17.55 / 13, 700, "Poppins"],
+        "paragraph-s-500": [13, 17.55 / 13, 500, "Poppins"],
+        "paragraph-s-400": [13, 17.55 / 13, 400, "Poppins"],
+        "paragraph-xs-500": [11, 14.85 / 11, 500, "Poppins"]
       }
     }
   }
@@ -49,10 +50,5 @@ const typographyPlugin = plugin(
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"]
-    }
-  },
   plugins: [typographyPlugin]
 }
