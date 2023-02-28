341a169262c9ef0af3481c00517311462d3780b4
Balazs Szabo <sazbak@gmail.com>
2023-02-28 19:54:23 +0100

    Put the functionality inside carousel instead of carousel-controller

--- src/components/carousel/carousel-controller.tsx
+++ src/components/carousel/carousel-controller.tsx
@@ -1,119 +1,27 @@
-import React, { useCallback, useEffect, useRef } from "react"
 import left_arrow from "assets/carousel/left-arrow.svg"
 import right_arrow from "assets/carousel/right-arrow.svg"
 import full_dot from "assets/carousel/full-dot.svg"
 import empty_dot from "assets/carousel/empty-dot.svg"
-import { FC, MutableRefObject } from "react"
+import { FC } from "react"
 
 type Props = {
   cardNumber: number
   selectedIndex: number
-  carousel: MutableRefObject<HTMLElement | null>
+  scrollCards: (direction: Direction) => void
 }
 
-enum Direction {
+export enum Direction {
   Forward = 1,
   Backward = -1
 }
 
 const CarouselController: FC<Props> = (props) => {
-  const cardWidth = useRef(0)
-  const cardGapX = useRef(0)
-  const cardScrollPositions = useRef([0])
-  const cardSnapPortBounds = useRef([0])
-  const activeCardIndex = useRef(0)
-  const snapDelay = 100
-
-  const scrollCards = useCallback(
-    (direction: Direction) => {
-      if (props.carousel) {
-        if (direction === Direction.Forward) {
-          activeCardIndex.current = Math.min(
-            props.cardNumber - 1,
-            activeCardIndex.current + 1
-          )
-        } else {
-          activeCardIndex.current = Math.max(0, activeCardIndex.current - 1)
-        }
-
-        props.carousel.current?.scrollTo(
-          cardScrollPositions.current[activeCardIndex.current],
-          0
-        )
-      }
-    },
-    [props.carousel, props.cardNumber]
-  )
-
-  useEffect(() => {
-    const carousel = props.carousel.current!
-    cardWidth.current = (carousel.children.item(0)! as HTMLElement).offsetWidth
-    if (carousel !== null) {
-      cardGapX.current = parseFloat(
-        window.getComputedStyle(carousel).gap.match(/\d+/)?.[0] ?? "0px"
-      )
-    }
-
-    cardScrollPositions.current = Array.from(
-      { length: props.cardNumber },
-      (_, index) => index * (1 + cardWidth.current + cardGapX.current)
-    )
-
-    cardSnapPortBounds.current = Array.from(
-      { length: props.cardNumber },
-      (_, index) => cardScrollPositions.current[index] - cardWidth.current / 2
-    )
-
-    let timeout: ReturnType<typeof setTimeout>
-
-    const handleScroll = () => {
-      clearTimeout(timeout)
-      if (
-        carousel.scrollLeft >
-        cardSnapPortBounds.current[activeCardIndex.current + 1]
-      ) {
-        timeout = setTimeout(() => {
-          props.carousel.current?.scrollTo(
-            cardScrollPositions.current[activeCardIndex.current + 1],
-            0
-          )
-        }, snapDelay)
-        activeCardIndex.current++
-      } else if (
-        carousel.scrollLeft <
-        cardSnapPortBounds.current[activeCardIndex.current]
-      ) {
-        timeout = setTimeout(() => {
-          props.carousel.current?.scrollTo(
-            cardScrollPositions.current[activeCardIndex.current - 1],
-            0
-          )
-        }, snapDelay)
-        activeCardIndex.current--
-      } else {
-        timeout = setTimeout(() => {
-          carousel.scrollTo(
-            cardScrollPositions.current[activeCardIndex.current],
-            0
-          )
-        }, snapDelay)
-      }
-      console.log(activeCardIndex)
-    }
-
-    props.carousel.current?.addEventListener("scroll", handleScroll)
-
-    return () => {
-      props.carousel.current?.removeEventListener("scroll", handleScroll)
-    }
-  }, [props.carousel, props.cardNumber])
-
   return (
     <div className="flex justify-between w-[151px] self-center mt-[34px] mb-[42px]">
       <img
         src={left_arrow}
         alt="Left arrow for carousel"
-        onClick={() => scrollCards(Direction.Backward)}
+        onClick={() => props.scrollCards(Direction.Backward)}
       />
       <div className="flex gap-x-[8px] self-center">
         {Array.from({ length: props.cardNumber }).map((_, index) =>
@@ -135,7 +43,7 @@ const CarouselController: FC<Props> = (props) => {
       <img
         src={right_arrow}
         alt="Right arrow for carousel"
-        onClick={() => scrollCards(Direction.Forward)}
+        onClick={() => props.scrollCards(Direction.Forward)}
       />
     </div>
   )
--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react"
+import React, { useCallback, useEffect, useRef } from "react"
 import { FC } from "react"
 import Card from "./card"
 import { CardProps } from "./card"
 import CarouselController from "./carousel-controller"
+import { Direction } from "./carousel-controller"
 
 type Props = {
   cards: CardProps[]
@@ -11,6 +12,96 @@ type Props = {
 const Carousel: FC<Props> = (props) => {
   const cards = props.cards
   const carouselRef = useRef(null)
+  const cardWidth = useRef(0)
+  const cardGapX = useRef(0)
+  const cardScrollPositions = useRef([0])
+  const cardSnapPortBounds = useRef([0])
+  const activeCardIndex = useRef(0)
+  const snapDelay = 100
+
+  const scrollCards = useCallback(
+    (direction: Direction) => {
+      if (carouselRef.current) {
+        if (direction === Direction.Forward) {
+          activeCardIndex.current = Math.min(
+            cards.length - 1,
+            activeCardIndex.current + 1
+          )
+        } else {
+          activeCardIndex.current = Math.max(0, activeCardIndex.current - 1)
+        }
+
+        ;(carouselRef.current as HTMLElement).scrollTo(
+          cardScrollPositions.current[activeCardIndex.current],
+          0
+        )
+      }
+    },
+    [cards.length]
+  )
+
+  useEffect(() => {
+    const carousel = carouselRef.current! as HTMLElement
+    cardWidth.current = (carousel.children.item(0)! as HTMLElement).offsetWidth
+    if (carousel !== null) {
+      cardGapX.current = parseFloat(
+        window.getComputedStyle(carousel).gap.match(/\d+/)?.[0] ?? "0px"
+      )
+    }
+
+    cardScrollPositions.current = Array.from(
+      { length: cards.length },
+      (_, index) => index * (1 + cardWidth.current + cardGapX.current)
+    )
+
+    cardSnapPortBounds.current = Array.from(
+      { length: cards.length },
+      (_, index) => cardScrollPositions.current[index] - cardWidth.current / 2
+    )
+
+    let timeout: ReturnType<typeof setTimeout>
+
+    const handleScroll = () => {
+      clearTimeout(timeout)
+      if (
+        carousel.scrollLeft >
+        cardSnapPortBounds.current[activeCardIndex.current + 1]
+      ) {
+        timeout = setTimeout(() => {
+          carousel.scrollTo(
+            cardScrollPositions.current[activeCardIndex.current + 1],
+            0
+          )
+        }, snapDelay)
+        activeCardIndex.current++
+      } else if (
+        carousel.scrollLeft <
+        cardSnapPortBounds.current[activeCardIndex.current]
+      ) {
+        timeout = setTimeout(() => {
+          carousel.scrollTo(
+            cardScrollPositions.current[activeCardIndex.current - 1],
+            0
+          )
+        }, snapDelay)
+        activeCardIndex.current--
+      } else {
+        timeout = setTimeout(() => {
+          carousel.scrollTo(
+            cardScrollPositions.current[activeCardIndex.current],
+            0
+          )
+        }, snapDelay)
+      }
+      console.log(activeCardIndex)
+    }
+
+    carousel.addEventListener("scroll", handleScroll)
+
+    return () => {
+      carousel.removeEventListener("scroll", handleScroll)
+    }
+  }, [cards.length])
 
   return (
     <div className="flex flex-col">
@@ -31,8 +122,8 @@ const Carousel: FC<Props> = (props) => {
       </div>
       <CarouselController
         cardNumber={cards.length}
-        selectedIndex={0}
-        carousel={carouselRef}
+        selectedIndex={activeCardIndex.current}
+        scrollCards={scrollCards}
       />
     </div>
   )
